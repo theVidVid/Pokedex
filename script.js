d2eba7102131615f49cba2e986caa3b6ef918699
Ian Vidaurre <ianjvidaurre@gmail.com>
2018-03-28 20:47:29 -0400

    fixed background url

--- script.js
+++ script.js
@@ -55,13 +55,13 @@ $.ajax({
 			let abilitiesList = getAbilities();
 			let pokemon = new Pokemon(name, image, hp, atk, def, pokeType, abilitiesList);	
 			pokeThrasher.pokeTeam.push(pokemon);
-			$("#name").append(pokeData.name);
-			$(".icon-display").children('img').attr('src','https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/101.png');
-			$(".hp").append(pokeData.stats[5].base_stat);
-			$(".atk").append(pokeData.stats[4].base_stat);
-			$(".def").append(pokeData.stats[3].base_stat);
-			$(".type").append(pokeData.types[0].type.name);
-			$(".abilities").children('#abilities').append(getAbilities(abilities));
+			// $("#name").append(pokeData.name);
+			// $(".icon-display").children('img').attr('src','https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/101.png');
+			// $(".hp").append(pokeData.stats[5].base_stat);
+			// $(".atk").append(pokeData.stats[4].base_stat);
+			// $(".def").append(pokeData.stats[3].base_stat);
+			// $(".type").append(pokeData.types[0].type.name);
+			// $(".abilities").children('#abilities').append(getAbilities(abilities));
 		}
 	});	
 }
@@ -89,13 +89,13 @@ $.ajax({
 			let abilitiesList = getAbilities();
 			let pokemon = new Pokemon(name, image, hp, atk, def, pokeType, abilitiesList);	
 			pokeThrasher.pokeTeam.push(pokemon);
-			// $(".name").append(pokeData.name);
+			// $("#name").append(pokeData.name);
 			// $(".icon-display").children('img').attr('src','https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/105.png');
 			// $(".hp").append(pokeData.stats[5].base_stat);
 			// $(".atk").append(pokeData.stats[4].base_stat);
 			// $(".def").append(pokeData.stats[3].base_stat);
 			// $(".type").append(pokeData.types[0].type.name);
-			// $(".abilities").append(getAbilities(abilities));
+			// $(".abilities").children('#abilities').append(getAbilities(abilities));
 		}
 	});	
 }
@@ -123,13 +123,13 @@ $.ajax({
 			let abilitiesList = getAbilities();
 			let pokemon = new Pokemon(name, image, hp, atk, def, pokeType, abilitiesList);	
 			pokeThrasher.pokeTeam.push(pokemon);
-			// $(".name").append(pokeData.name);
-			// $(".icon-display").children('img').attr('src','https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/169.png');
-			// $(".hp").append(pokeData.stats[5].base_stat);
-			// $(".atk").append(pokeData.stats[4].base_stat);
-			// $(".def").append(pokeData.stats[3].base_stat);
-			// $(".type").append(pokeData.types[0].type.name);
-			// $(".abilities").append(getAbilities(abilities));
+			$("#name").append(pokeData.name);
+			$(".icon-display").children('img').attr('src','https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/169.png');
+			$(".hp").append(pokeData.stats[5].base_stat);
+			$(".atk").append(pokeData.stats[4].base_stat);
+			$(".def").append(pokeData.stats[3].base_stat);
+			$(".type").append(pokeData.types[0].type.name);
+			$(".abilities").children('#abilities').append(getAbilities(abilities));
 		}
 	});	
 }
